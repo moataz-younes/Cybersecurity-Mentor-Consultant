52a0b92307b645b8e961f5bb52a5d5e0b615aa84
Moataz yones <153294476+moataz-younes@users.noreply.github.com>
2025-07-24 16:08:57 +1300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/project-bolt-sb1-kdtuimrs/',
+  base: '/Cybersecurity-Mentor-Consultant/',
   plugins: [react()],
-});
+})
