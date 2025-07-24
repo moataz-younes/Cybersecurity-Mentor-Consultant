290dc084ab033ba00209fd4be2fa2576ae6279b1
Moataz yones <153294476+moataz-younes@users.noreply.github.com>
2025-07-24 16:00:00 +1300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // أضف هذا
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Cybersecurity-Mentor-Consultant"> {/* هذا مهم جدًا */}
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
+
